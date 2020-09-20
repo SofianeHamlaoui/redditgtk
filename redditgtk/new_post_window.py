f96f3207226f2d30e3779e7dedeef2e27b600a95
Gabriele Musco <gabmus@disroot.org>
2020-09-20 09:07:08 +0200

    wired send button sensitivity in new post window

--- redditgtk/new_post_window.py
+++ redditgtk/new_post_window.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk, Gdk, GtkSource, GObject
+from os.path import isfile
 
 
 class NewPostWindow(Gtk.Window):
@@ -17,43 +18,50 @@ class NewPostWindow(Gtk.Window):
             '/org/gabmus/redditgtk/ui/new_post_window.glade'
         )
 
+        self.title_entry = self.builder.get_object('title_entry')
+        self.link_entry = self.builder.get_object('link_entry')
+        self.file_chooser_btn = self.builder.get_object('file_chooser_btn')
+        self.text_source_view_container = self.builder.get_object(
+            'text_source_view_container'
+        )
+
         self.set_titlebar(self.builder.get_object('headerbar'))
         self.add(self.builder.get_object('inner_box'))
 
         hide = []
         show = []
         if self.post_type == 'text':
-            hide = ['link_entry', 'file_chooser_btn']
-            show = ['text_source_view_container']
+            hide = [self.link_entry, self.file_chooser_btn]
+            show = [self.text_source_view_container]
         elif self.post_type == 'link':
-            hide = ['file_chooser_btn', 'text_source_view_container']
-            show = ['link_entry']
+            hide = [self.file_chooser_btn, self.text_source_view_container]
+            show = [self.link_entry]
         else:  # media
-            hide = ['text_source_view_container', 'link_entry']
-            show = ['file_chooser_btn']
+            hide = [self.text_source_view_container, self.link_entry]
+            show = [self.file_chooser_btn]
 
-        for wname in hide:
-            w = self.builder.get_object(wname)
+        for w in hide:
             w.set_visible(False)
             w.set_no_show_all(True)
-        for wname in show:
-            w = self.builder.get_object(wname)
+        for w in show:
             w.set_visible(True)
             w.set_no_show_all(False)
 
-        if 'text_source_view_container' in show:
+        self.source_buffer = None
+        if self.text_source_view_container in show:
             source_style_scheme_manager = \
                 GtkSource.StyleSchemeManager.get_default()
             source_lang_manager = GtkSource.LanguageManager.get_default()
             source_lang_md = source_lang_manager.get_language('markdown')
-            source_buffer = GtkSource.Buffer()
+            self.source_buffer = GtkSource.Buffer()
             color_scheme = 'oblivion'
-            source_buffer.set_style_scheme(
+            self.source_buffer.set_style_scheme(
                 source_style_scheme_manager.get_scheme(color_scheme)
             )
-            source_buffer.set_language(source_lang_md)
+            self.source_buffer.set_language(source_lang_md)
             source_view = self.builder.get_object('source_view')
-            source_view.set_buffer(source_buffer)
+            source_view.set_buffer(self.source_buffer)
+            self.source_buffer.connect('changed', self.on_input)
 
         self.select_subreddit_combobox = self.builder.get_object(
             'select_subreddit_combobox'
@@ -70,9 +78,63 @@ class NewPostWindow(Gtk.Window):
         self.cancel_btn.connect('clicked', lambda *args: self.destroy())
         # TODO: send button (in/)sensitive based on content filled
         self.send_btn = self.builder.get_object('send_btn')
+        self.send_btn.set_sensitive(False)
         self.send_btn.connect('clicked', self.on_send)
+
+        for entry in (
+                self.title_entry,
+                self.link_entry,
+                self.select_subreddit_combobox
+        ):
+            entry.connect('changed', self.on_input)
+        self.file_chooser_btn.connect(
+            'selection-changed',
+            self.on_input
+        )
+
+        self.accel_group = Gtk.AccelGroup()
+        self.accel_group.connect(
+            *Gtk.accelerator_parse('Escape'), Gtk.AccelFlags.VISIBLE,
+            lambda *args: self.close()
+        )
+        self.add_accel_group(self.accel_group)
 
     def on_send(self, *args):
         # TODO: send post
         # TODO: verify that post has been sent before closing, else show error
         self.destroy()
+
+    def on_input(self, *args):
+        self.send_btn.set_sensitive(
+            self.get_selected_subreddit() is not None and
+            len(self.title_entry.get_text()) > 0 and
+            (
+                self.post_type != 'text' or
+                len(self.get_sourcebuffer_text().strip()) > 0
+            ) and
+            (
+                self.post_type != 'link' or
+                len(self.link_entry.get_text().strip()) > 0
+            ) and
+            (
+                self.post_type != 'media' or
+                isfile(self.get_selected_media())
+            )
+        )
+
+    def get_selected_media(self):
+        return self.file_chooser_btn.get_filename()
+
+    def get_sourcebuffer_text(self):
+        return self.source_buffer.get_text(
+            self.source_buffer.get_start_iter(),
+            self.source_buffer.get_end_iter(),
+            True
+        ).strip() if self.source_buffer is not None else ''
+
+    def get_selected_subreddit(self):
+        iter = self.select_subreddit_combobox.get_active_iter()
+        if iter is not None:
+            return self.select_subreddit_combobox.get_model()[iter][1]
+        else:
+            return None
